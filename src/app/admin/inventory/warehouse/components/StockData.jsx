25af97dd4304a195faa2da82f7392ff4c2a7ea48
unknown <MuhammadEhtisham60>
2026-01-31 14:43:04 +0500

    fix

--- src/app/admin/inventory/warehouse/components/StockData.jsx
+++ src/app/admin/inventory/warehouse/components/StockData.jsx
@@ -11,7 +11,7 @@ const StokeCard = ({
       <CardBody>
         <div className="d-flex align-items-center justify-content-between">
           <div>
-            <CardTitle as={'h4'} className="mb-2 d-flex align-items-center gap-2">
+            <CardTitle as={'h5'} className="mb-2 d-flex align-items-center ">
               {title}
             </CardTitle>
             <p className="text-muted fw-medium fs-22 mb-0">
@@ -25,18 +25,18 @@ const StokeCard = ({
                 </> : <span className="fs-12">(Items)</span>}
             </p>
           </div>
-          <div>
+          {/* <div>
             <div className="avatar-md bg-primary bg-opacity-10 rounded flex-centered">
               <IconifyIcon width={32} height={32} icon={icon} className="fs-32 text-primary" />
             </div>
-          </div>
+          </div> */}
         </div>
       </CardBody>
     </Card>;
 };
 const StockData = () => {
   return <Row>
-      {stokeData.map((item, idx) => <Col md={6} xl={3} key={idx}>
+      {stokeData.map((item, idx) => <Col md={6} xl={6} key={idx}>
           <StokeCard {...item} />
         </Col>)}
     </Row>;
